a563ae1c2397a7fe1d12261d4b6138417e606f6e
jang970 <jang970@aucklanduni.ac.nz>
2024-06-24 21:43:56 +1200

    implement user repository

---
+++ backend/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+namespace backend.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+        Task<bool> UserExistsAsync(int id);
+    }
+}
