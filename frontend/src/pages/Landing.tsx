2e838e414bd59929df9adb466847d2d7e2c1196e
jang970 <jang970@aucklanduni.ac.nz>
2024-07-02 17:57:20 +1200

    implement barebone landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,11 +1,23 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 //todo: this will contain the sign up and login components
 const Landing: React.FC = () => {
   return (
-    <Box>
-      <Typography variant="h1">Welcome to the Landing Page</Typography>
-    </Box>
+    <Stack direction="column" alignItems="center" gap={4}>
+      <Typography variant="h1">App Name Here</Typography>
+      <Stack bgcolor="beige" width="50%" gap={3} p={6} borderRadius="0.5rem">
+        <TextField label="Email" size="small" type="email" required />
+        <TextField label="Password" size="small" type="password" required />
+        <Stack direction="row" gap={2}>
+          <Button variant="contained" fullWidth>
+            Sign In
+          </Button>
+          <Button variant="contained" fullWidth>
+            Register
+          </Button>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
 
