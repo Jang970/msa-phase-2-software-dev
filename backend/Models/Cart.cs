8ca06fe46b2a6b7a0b18ee10db3c09c51408d067
Jang970 <jang970@aucklanduni.ac.nz>
2024-08-02 22:32:11 +1200

    delete first backend

--- backend/Models/Cart.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Models
-{
-    public class Cart
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
-    }
-}
